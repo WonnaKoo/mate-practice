70659c785fb1b21e044802ff755b531f72b65d08
Tymur Yermolenko <wonnnakoo@gmail.com>
2023-10-05 15:06:56 +0300

    add models and add path to urls.py

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,2 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
